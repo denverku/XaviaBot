8ab5325b750683e37f12235ec47ed89c045f5b16
denverku <magicph26@gmail.com>
2023-01-20 13:55:03 +0800

    Update checkAppstate.js

--- core/var/modules/checkAppstate.js
+++ core/var/modules/checkAppstate.js
@@ -1,4 +1,4 @@
-import replitDB from "@replit/database";
+
 import { resolve as resolvePath } from 'path';
 import { readFileSync, writeFileSync } from "fs";
 import { execSync } from "child_process";
@@ -8,7 +8,7 @@ import { execSync } from "child_process";
  */
 async function checkAppstate(APPSTATE_PATH, APPSTATE_PROTECTION) {
     const logger = global.modules.get('logger');
-    const { isReplit, isGlitch } = global.modules.get('environments.get');
+    /*const { isReplit, isGlitch } = global.modules.get('environments.get');*/
 
     let objAppState;
     APPSTATE_PATH = resolvePath(APPSTATE_PATH);
@@ -22,13 +22,13 @@ async function checkAppstate(APPSTATE_PATH, APPSTATE_PROTECTION) {
     let appState = readFileSync(APPSTATE_PATH, 'utf8');
     appState = appState.startsWith("\"") ? JSON.parse(appState) : appState; // fixed...
     logger.custom(getLang('modules.checkAppstate.noProtection'), 'LOGIN');
-        objAppState = await getAppStateNoProtection(APPSTATE_PATH, appState, isReplit, isGlitch);
+        objAppState = await getAppStateNoProtection(APPSTATE_PATH, appState);
     
 
     return objAppState;
 }
 
-async function getAppStateNoProtection(APPSTATE_PATH, appState, isReplit, isGlitch) {
+async function getAppStateNoProtection(APPSTATE_PATH, appState) {
     const logger = global.modules.get('logger');
     const aes = global.modules.get('aes');
 
@@ -38,7 +38,7 @@ async function getAppStateNoProtection(APPSTATE_PATH, appState, isReplit, isGlit
         if (isJSON(appState)) {
             objAppState = JSON.parse(appState);
             if (objAppState.length == 0) {
-                if (!isGlitch || !isExists(resolvePath('.data', 'appstate.json'))) throw getLang('modules.checkAppstate.error.invalid');
+                if (!isExists(resolvePath('.data', 'appstate.json'))) throw getLang('modules.checkAppstate.error.invalid');
 
                 objAppState = JSON.parse(readFileSync(resolvePath('.data', 'appstate.json'), 'utf8'));
                 writeFileSync(APPSTATE_PATH, JSON.stringify(objAppState, null, 2), 'utf8');
